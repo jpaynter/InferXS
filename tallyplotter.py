a84d96b5c138c63f70b3c427d05307a17b240c17
Will Boyd <wbinventor@gmail.com>
2013-10-31 22:27:13 -0400

    Updated tallyplotter for more general use

--- tallyplotter.py
+++ tallyplotter.py
@@ -5,14 +5,17 @@ import time
 
 # User parameters
 sleep_interval = 0.5    # seconds
-batch_start = 20
+batch_start = 260
 batch_stop = 1000
 batch_interval = 10
 
-score1 = 'flux'
-score2 = 'nu-fission'
-mesh_x_dim = 5
-mesh_y_dim = 5
+directory = 'Fuel-1.6wo-CRD/'
+
+score1 = 'nu-fission'
+score2 = 'absorption'
+energy = 1
+mesh_x_dim = 17
+mesh_y_dim = 17
 num_groups = 2
 
 
@@ -21,73 +24,57 @@ num_groups = 2
 ################################################################################
 
 # Instantiate statepoint for the first batch of tally data
-sp = StatePoint('pincell/statepoint.' + str(batch_start) + '.h5')
+sp = StatePoint(directory + 'statepoint.' + str(batch_start) + '.h5')
 sp.read_results()
 tallyid = 1
-flux_data = sp.extract_results(tallyid, score1)
-nu_fiss_data = sp.extract_results(tallyid, score2)
+tally1_data = sp.extract_results(tallyid, score1)
+tally2_data = sp.extract_results(tallyid, score2)
 
-flux_means = flux_data['mean']
-flux_std_dev = flux_data['CI95']
-nu_fiss_means = nu_fiss_data['mean']
-nu_fiss_std_dev = np.nan_to_num(nu_fiss_data['CI95'])
+tally1_means = flux_data['mean']
+tally1_std_dev = np.nan_to_num(flux_data['CI95'])
+tally2_means = nu_fiss_data['mean']
+tally2_std_dev = np.nan_to_num(nu_fiss_data['CI95'])
 
 # Reshape to grid with energy as third index
-flux_means = np.reshape(flux_means,(mesh_x_dim, mesh_y_dim, num_groups))
-flux_std_dev = np.reshape(flux_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
-nu_fiss_means = np.reshape(nu_fiss_means,(mesh_x_dim, mesh_y_dim, num_groups))
-nu_fiss_std_dev = np.reshape(nu_fiss_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
-
-flux_variance = np.power(flux_std_dev, 2)
-nu_fiss_variance = np.power(nu_fiss_std_dev, 2)
-
-means = (nu_fiss_means / flux_means) + (nu_fiss_means / (np.power(flux_means,3))) * flux_variance
-variance = (nu_fiss_variance / (np.power(flux_means, 2))) + \
-           ((np.power(nu_fiss_means,2) * flux_variance) / (np.power(flux_means, 4)))
-variance = np.nan_to_num(variance)
-std_dev = np.sqrt(variance)
-
-print 'max std dev = ' + str(np.max(std_dev))
-print 'avg std dev = ' + str(np.average(std_dev[np.nonzero(std_dev)]))
+tally1_means = np.reshape(flux_means,(mesh_x_dim, mesh_y_dim, num_groups))
+tally1_std_dev = np.reshape(flux_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
+tally2_means = np.reshape(nu_fiss_means,(mesh_x_dim, mesh_y_dim, num_groups))
+tally2_std_dev = np.reshape(nu_fiss_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
 
 # Instantiate a PyPlot figure and turn on Matplotlib's interactive mode
 fig = plt.figure(1)
 plt.ion()
 
-# Plot fast flux tally mesh in top left corner
-plt.subplot(221)
-img = plt.imshow(std_dev[:,:,0], interpolation='nearest', animated=True)
-plt.title('Fast Flux')
+# Plot tally1 in top left corner
+plt.subplot(121)
+img = plt.imshow(tally1_means[:,:,energy-1], interpolation='nearest', animated=True)
 plt.colorbar()
 
-# Plot thermal flux tally mesh in top right corner
-plt.subplot(222)
-img = plt.imshow(std_dev[:,:,1], interpolation='nearest', animated=True)
-plt.title('Thermal Flux')
+# Plot tally2 in top right corner
+plt.subplot(122)
+img = plt.imshow(tally2_means[:,:,energy-1], interpolation='nearest', animated=True)
 plt.colorbar()
 
-# Plot fast flux std dev in lower left corner
+# Plot tally1 std dev in lower left corner
 plt.subplot(223)
 batches = [10]
-std_dev_fast = [np.max(std_dev[:,:,0])]
-std_dev_fast_ideal = [std_dev_fast[0]]
-plt.plot(batches, std_dev_fast, animated=True, color='blue')
-plt.plot(batches, std_dev_fast_ideal, animated=True, color='green')
+std_dev_tally1 = [np.max(tally1_std_dev[:,:,group-1])]
+std_dev_tally1_ideal = [std_dev_tally1[0]]
+plt.plot(batches, std_dev_tally1, animated=True, color='blue')
+plt.plot(batches, std_dev_tally1_ideal, animated=True, color='green')
 plt.xlabel('Batch #')
 plt.ylabel('CI95')
 plt.legend(['Sample', 'Ideal'])
-plt.title('Fast Flux Uncertainty')
 
-# Plot therm flux std dev in lower right corner
+# Plot tally2 std dev in lower right corner
 plt.subplot(224)
-std_dev_therm = [np.max(std_dev[:,:,1])]
-std_dev_therm_ideal = [std_dev_therm[0]]
-plt.plot(batches, std_dev_therm, animated=True, color='blue')
-plt.plot(batches, std_dev_therm_ideal, animated=True, color='green')
+std_dev_tally2 = [np.max(std_dev[:,:,groups-1])]
+std_dev_tally2_ideal = [std_dev_therm[0]]
+plt.plot(batches, std_dev_tally2, animated=True, color='blue')
+plt.plot(batches, std_dev_tally2_ideal, animated=True, color='green')
 plt.xlabel('Batch #')
 plt.ylabel('Std. Dev.')
 plt.legend(['Sample', 'Ideal'])
-plt.title('Thermal Flux Uncertainty')
 
 # Draw all of the subplots and sleep
 plt.draw()
@@ -97,58 +84,45 @@ time.sleep(sleep_interval)
 for batch in range(batch_start, batch_stop+batch_interval, batch_interval):
     
     # Instantiate statepoint for the this batch of tally data
-    sp = StatePoint('pincell/statepoint.' + str(batch) + '.h5')
+    sp = StatePoint(directory+'statepoint.' + str(batch) + '.h5')
     sp.read_results()
     tallyid = 1
-    flux_data = sp.extract_results(tallyid, score1)
-    nu_fiss_data = sp.extract_results(tallyid, score2)
+    tally1_data = sp.extract_results(tallyid, score1)
+    tally2_data = sp.extract_results(tallyid, score2)
 
-    flux_means = flux_data['mean']
-    flux_std_dev = flux_data['CI95']
-    nu_fiss_means = nu_fiss_data['mean']
-    nu_fiss_std_dev = np.nan_to_num(nu_fiss_data['CI95'])
-
-    flux_variance = np.power(flux_std_dev, 2)
-    nu_fiss_variance = np.power(nu_fiss_std_dev, 2)
-
-    means = (nu_fiss_means / flux_means) + (nu_fiss_means / (np.power(flux_means,3))) * flux_variance
-    variance = (nu_fiss_variance / (np.power(flux_means, 2))) + \
-           ((np.power(nu_fiss_means,2) * flux_variance) / (np.power(flux_means, 4)))
-    variance = np.nan_to_num(variance)
-    std_dev = np.sqrt(variance)
-
-    print 'max std dev = ' + str(np.max(std_dev))
-    print 'avg std dev = ' + str(np.average(std_dev[np.nonzero(std_dev)]))
-    print 'max fiss xs = ' + str(np.max(means))
-    print 'avg fiss xs = ' + str(np.average(means[np.nonzero(means)]))
-
+    tally1_means = flux_data['mean']
+    tally2_std_dev = np.nan_to_num(flux_data['CI95'])
+    tally1_means = nu_fiss_data['mean']
+    tally2_std_dev = np.nan_to_num(nu_fiss_data['CI95'])
 
     # Reshape to grid with energy as third index
-    means = np.reshape(means,(mesh_x_dim,mesh_y_dim,num_groups))
-    std_dev = np.reshape(std_dev,(mesh_x_dim,mesh_y_dim,num_groups))
+    tally1_means = np.reshape(tally1_means,(mesh_x_dim, mesh_y_dim, num_groups))
+    tally1_std_dev = np.reshape(tally1_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
+    tally2_means = np.reshape(tally2_means,(mesh_x_dim, mesh_y_dim, num_groups))
+    tally2_std_dev = np.reshape(tally2_std_dev,(mesh_x_dim, mesh_y_dim, num_groups))
     
-    # Plot fast flux tally mesh in top left corner
+    # Plot tally1 in top left corner
     plt.subplot(221)
-    plt.imshow(std_dev[:,:,0], interpolation='nearest')
+    plt.imshow(std_dev[:,:,group-1], interpolation='nearest')
 
-    # Plot thermal flux tally mesh in top right corner
+    # Plot tally2 in top right corner
     plt.subplot(222)
-    plt.imshow(std_dev[:,:,1], interpolation='nearest')
+    plt.imshow(std_dev[:,:,group-1], interpolation='nearest')
 
-    # Plot fast flux std dev in lower left corner
+    # Plot tally1 std dev in lower left corner
     plt.subplot(223)
     batches.append(batch)
-    std_dev_fast.append(np.max(std_dev[:,:,0]))
-    std_dev_fast_ideal.append(std_dev_fast[0] / np.sqrt(batch))
-    plt.plot(batches, std_dev_fast, color='blue')
-    plt.plot(batches, std_dev_fast_ideal, color='green')
+    std_dev_tally1.append(np.max(std_dev_tally1[:,:,group-1]))
+    std_dev_tally1_ideal.append(std_dev_tally1[0] / np.sqrt(batch))
+    plt.plot(batches, std_dev_tally1, color='blue')
+    plt.plot(batches, std_dev_fast_tally1, color='green')
 
-    # Plot therm flux std dev in lower right corner
+    # Plot tally2 std dev in lower right corner
     plt.subplot(224)
-    std_dev_therm.append(np.max(std_dev[:,:,1]))
-    std_dev_therm_ideal.append(std_dev_therm[0] / np.sqrt(batch))
-    plt.plot(batches, std_dev_therm, color='blue')
-    plt.plot(batches, std_dev_therm_ideal, color='green')
+    std_dev_tally2.append(np.max(tally2_std_dev[:,:,group-1]))
+    std_dev_tally2_ideal.append(std_dev_tally2[0] / np.sqrt(batch))
+    plt.plot(batches, std_dev_tally2, color='blue')
+    plt.plot(batches, std_dev_tally2_ideal, color='green')
 
     # Update all of the subplots and sleep
     plt.draw()
